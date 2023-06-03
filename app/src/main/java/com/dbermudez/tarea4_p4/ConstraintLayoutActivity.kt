476995123750673ecc0ad77886d2f0307c643fbf
BermudezD33 <51296462+BermudezD33@users.noreply.github.com>
2023-06-02 20:47:05 -0600

    commit basic input

---
+++ app/src/main/java/com/dbermudez/tarea4_p4/ConstraintLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.dbermudez.tarea4_p4
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConstraintLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
